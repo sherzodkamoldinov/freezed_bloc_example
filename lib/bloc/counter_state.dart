7093a141bdac287af0691a272057981e8f4dce44
sherzodkamoldinov <sherzodkamoldinov1999@gmail.com>
2023-02-08 13:29:43 +0500

    added BLOC and freezed package for BLOC

---
+++ lib/bloc/counter_state.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_bloc.dart';
+
+@freezed
+class CounterState with _$CounterState {
+  const factory CounterState.initial() = _CounterInitialState;
+  const factory CounterState.loading() = _CounterLoadingState;
+  const factory CounterState.loaded({required int counter}) = _CounterLoadedState;
+}
+
+// abstract class CounterState extends Equatable {
+//   const CounterState();
+
+//   @override
+//   List<Object?> get props => [];
+// }
+
+// class CounterInitialState extends CounterState {}
+
+// class CounterLoadingState extends CounterState {}
+
+// class CounterLoadedState extends CounterState {
+//   final int counter;
+//   const CounterLoadedState({required this.counter});
+
+//   @override
+//   List<Object?> get props => [counter];
+// }
--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_bloc_example/bloc/counter_bloc.dart';
 
-class CounterPage extends StatelessWidget {
-const CounterPage({ Key? key }) : super(key: key);
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Freezed with Bloc'),
+        centerTitle: true,
+      ),
+      body: Center(child: _CounterText()),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<CounterBloc>().add(const CounterEvent.increment()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 20),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterBloc>().add(const CounterEvent.reset()),
+            tooltip: 'Reset',
+            child: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CounterText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<CounterBloc>().state;
+    return state.when(
+      initial: () => const FlutterLogo(size: 120),
+      loading: () => const CircularProgressIndicator(),
+      loaded: (counter) => Text('$counter', style: Theme.of(context).textTheme.displayMedium),
+    );
+    // if (state is CounterInitialState) {
+    //   return const FlutterLogo(size: 120);
+    // } else if (state is CounterLoadingState) {
+    //   return const CircularProgressIndicator();
+    // } else if (state is CounterLoadedState) {
+    //   return Text('${state.counter}', style: Theme.of(context).textTheme.displayMedium);
+    // }
+    // return const SizedBox.shrink();
   }
 }
