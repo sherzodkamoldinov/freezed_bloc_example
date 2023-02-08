fe7b82ff878c28b0c954fd26f21a08e3b72f33b4
sherzodkamoldinov <sherzodkamoldinov1999@gmail.com>
2023-02-08 12:16:30 +0500

    first init

---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatelessWidget {
+const CounterPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
